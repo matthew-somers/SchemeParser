3d27377e1ff2837367ddd3c3a464cbf74a9837e1
Matthew Somers <matthew.somers.hi@gmail.com>
2013-11-20 16:48:40 -0800

    added output file

--- test.lisp
+++ test.lisp
@@ -1,147 +1,85 @@
-; Differentiate an infix polynomial with terms of the form +cax^n
-; where c is an optional integer constant and a is an optional
-; variable other than x, and optional exponent n is an integer > 0.
-; Example: (3 a x ^ 5 + b x ^ 4 + 2 x ^ 3 + 6 x ^ 2 + 3 x + 7)
-; Differentiate with respect to var, for example x.
-; The derivative will be in the same infix form.
-;
-; Assumptions:
-;   The polynomial is in the "canonical" form as described above.
-;   There is no error checking of the form. In particular, only
-;   the variable we're differentiating with respect to (such as x)
-;   can have an exponent. Any deviation from canonical form may
-;   cause a runtime exception.
-;
-; CS 152 Programming Language Paradigms
-; Department of Computer Science
-; San Jose State University
-; Fall 2013
-; Instructor: Ron Mak
+(define member? 
+(lambda (item lst) 
+(cond 
+((null? lst) #f) 
+((equal? item (car lst)) #t) 
+(else (member? item (cdr lst))) 
+))) 
+(member? 3 '(1 2 3))
+(member? 'b '(a (b c) d))
 
-; Find the derivative of polynomial poly with repect to variable var.
-; The polynomial must be in canonical infix form.
-; First "terminize" the polynomial.
-; Example:
-;  (terminize '(3 a x ^ 5 + b x ^ 4 + 2 x ^ 3 + 6 x ^ 2 + 3 x + 7))
-;         ==> ((3 a x ^ 5) (b x ^ 4) (2 x ^ 3) (6 x ^ 2) (3 x) (7)))
-; Note that var is a free variable in local procedure deriv-term.
-; At run time, deriv-term obtains the value of var from its closure,
-; and it is mapped over each sublist in the terminized polynomial. 
-; The result is a list of sublists, each sublist representing the derivative 
-; of the corresponding term.
-; Example: 
-;  (map deriv-term '((3 a x ^ 5)   (b x ^ 4) (2 x ^ 3)  (6 x ^ 2) (3 x) (7))))
-;              ==> ((15 a x ^ 4) (4 b x ^ 3) (6 x ^ 2) (12 x)     (3)   (0))
-; Example: 
-;  (deriv '(3 a x ^ 5 +   b x ^ 4 + 2 x ^ 3 +  6 x ^ 2 + 3 x + 7) 'x)
-;     ==> (15 a x ^ 4 + 4 b x ^ 3 + 6 x ^ 2 + 12 x     + 3)
-(define deriv
-  (lambda (poly var)
-    (let* ((terms (terminize poly)) ; "terminize" the polynomial
-           (deriv-term              ; local procedure deriv-term 
-             (lambda (term)
-               (cond
-                 ((null? term) '())
-                 ((not (member? var term)) '(0))           ; deriv = 0
-                 ((not (member? '^ term)) (upto var term)) ; deriv = coeff
-                 (else (deriv-term-expo term var))         ; handle exponent
-             )))
-           (diff (map deriv-term terms)))   ; map deriv-term over the terms
-      (remove-trailing-plus (polyize diff)) ; finalize the answer
-)))
+(define remove-last 
+(lambda (item lst) 
+(cond 
+((null? lst) '()) 
+((and (equal? item (car lst)) (not (member? item (cdr lst)))) (cdr lst)) 
+(else (cons (car lst) (remove-last item (cdr lst))))) 
+)) 
+(remove-last 'a '(b a n a n a s)) 
+(remove-last '(a b) '(a b (a b) a b (b a) a b (a b) a b))
 
-; Differentiate a single term that contains the var
-; raised to a power (i.e., there's an exponent).
-; If there are two adjacent integer constants in the new
-; coefficient, replace them by their product.
-; Example:  (deriv-term-expo '(3 a x ^ 5) 'x) ==> (15 a x ^ 4)
-(define deriv-term-expo
-  (lambda (term var)
-    (let* ((coeff (upto var term))    ; get the coefficient
-           (rev-term (reverse term))  ; reverse so that we can
-           (expo (car rev-term))      ; get the exponent at the end
-           (new-expo (sub1 expo))     ; new exponent
-           (new-coeff (if (number? (car coeff))
-                          (cons (* (car coeff) expo) (cdr coeff)) ; product
-                          (cons expo coeff))))
-                          
-      ; The derivative:
-      (if (= new-expo 1)
-          (append new-coeff (list var))
-          (append new-coeff (list var '^ new-expo)))
-)))
+(define same-structure? 
+(lambda (x y) 
+(cond 
+((and (null? x) (null? y)) #t) 
+((null? x) #f) 
+((null? y) #f) 
+((and (pair? (car x)) (pair? (car y))) 
+(and (same-structure? (car x) (car y)) 
+(same-structure? (cdr x) (cdr y)))) 
+(else (and (same-type? (car x) (car y)) 
+(same-structure? (cdr x) (cdr y))))) 
+)) 
+(define float? 
+(lambda (x) 
+(and (real? x) (not (integer? x))) 
+)) 
+(define same-type? 
+(lambda (x y) 
+(or (and (symbol? x) (symbol? y)) 
+(and (integer? x) (integer? y)) 
+(and (float? x) (float? y)) 
+(and (boolean? x) (boolean? y)) 
+(and (char? x) (char? y)) 
+(and (string? x) (string? y))) 
+)) 
+(same-structure? '(1 (a (b 3.14) ((c)))) '(3 (z (x 1.23) ((q)))))
+(same-structure? '(1 (a (b 3.14) ((c)))) '(3 (z (x 3) ((q)))))
+(same-structure? '(1 2 3 4 5) '(5 4 3 2))
+(same-structure? '() '()) 
+(same-structure? '(("hello") "world") '(("good-bye") "sam"))
 
-; Convert an infix polynomial into a list of sublists,
-; where each sublist is a term.
-; Example:  (terminize '(3 a x ^ 5 + b x ^ 4 + 2 x ^ 3 + 6 x ^ 2 + 3 x + 7))
-;                  ==> ((3 a x ^ 5) (b x ^ 4) (2 x ^ 3) (6 x ^ 2) (3 x) (7)))
-(define terminize
-  (lambda (poly)
-    (cond
-      ((null? poly) '())
-      (else (cons (upto '+ poly) (terminize (after '+ poly))))
-)))
+(define sandwich-first 
+(lambda (a b lst) 
+(cond 
+((null? lst) '()) 
+((null? (cdr lst)) lst) 
+((and (equal? b (car lst)) (equal? b (cadr lst))) 
+(append (list b a b) (cddr lst))) 
+(else (cons (car lst) (sandwich-first a b (cdr lst))))) 
+)) 
+(sandwich-first 'meat 'bread '(bread bread)) 
+(sandwich-first 'meat 'bread '())
+(sandwich-first 'meat 'bread '(meat meat))
 
-; Convert a list of term sublists to an infix polynomial.
-; Do not include any + 0 term.
-; There may be an extra + at the end.
-; Example: (polyize '((15 a x ^ 4) (4 b x ^ 3) (6 x ^ 2) (12 x) (3) (0)))
-;                 ==> (15 a x ^ 4 + 4 b x ^ 3 + 6 x ^ 2 + 12 x + 3 +)
-(define polyize
-  (lambda (terms)
-    (cond
-      ((null? terms) '())
-      ((equal? '(0) (car terms)) '())
-      ((null? (cdr terms)) (car terms))
-      (else (append (car terms) '(+) (polyize (cdr terms))))
-)))
+(define min-to-head
+(lambda (lst) 
+(cond 
+((null? lst) '()) 
+((null? (cdr lst)) lst) 
+(else (let* ((new-lst (min-to-head (cdr lst))) 
+(second (car new-lst))) 
+(if (> (car lst) second) 
+(cons second (cons (car lst) (cdr new-lst))) 
+lst))) 
+))) 
+(min-to-head '(5 8 1 0 6 2 1 9))
 
-; Return the polynomial without any extra + at the end. 
-; If the polynomial is empty, return (0).
-(define remove-trailing-plus
-  (lambda (poly)
-    (if (null? poly)
-        '(0)
-        (let ((rev-poly (reverse poly)))
-          (if (equal? '+ (car rev-poly))
-              (reverse (cdr rev-poly))
-              (reverse rev-poly))
-))))
-
-; Return #t if the given item is a top-level item of the given list.
-; Else return #f.
-(define member?
-  (lambda (item lst)
-    (cond
-      ((null? lst) #f)
-      ((equal? item (car lst)) #t)
-      (else (member? item (cdr lst)))
-)))
-
-; Return a list consisting of the items of the given list
-; up to the first occurrence of the given item.
-; Used to extract the coefficient of a term.
-; Return the original list if the given item isn't in the list.
-; Example: (upto 'x '(3 a x ^ 5)) ==> (3 a)
-(define upto
-  (lambda (item lst)
-    (cond
-      ((null? lst) '())
-      ((equal? item (car lst)) '())
-      (else (cons (car lst) (upto item (cdr lst))))
-)))
-
-; Return a list consisting of the items of the given list
-; after the first occurrence of the given item.
-; Used to extract the exponent of a term.
-; Return the empty list if the given item isn't in the list.
-; Example: (after 'x '(3 a x ^ 5)) ==> (^ 5)
-(define after
-  (lambda (item lst)
-    (cond
-      ((null? lst) '())
-      ((equal? item (car lst)) (cdr lst))
-      (else (after item (cdr lst)))
-)))
-
-(deriv '(3 a x ^ 5 + b x ^ 4 + 2 x ^ 3 + 6 x ^ 2 + 3 x + 7) 'x)
+(define sort 
+(lambda (lst) 
+(if (null? lst) 
+'() 
+(let ((mth (min-to-head lst))) 
+(cons (car mth) (sort (cdr mth))))) 
+)) 
+(sort '(5 8 1 0 6 2 1 9))
